889453e0ae77c29bf965d5658b746a5b46b32c10
HansBambel <HansBambel@gmx.net>
2018-10-31 15:55:14 +0100

    a start

--- sparse_autoencoder.py
+++ sparse_autoencoder.py
@@ -1,14 +1,18 @@
 import numpy as np
-import math
 
 
 def sigmoid(x):
-    return 1 / (1 + math.exp(-x))
+    return 1 / (1 + np.exp(-x))
+
+
+def derivedSigmoid(x):
+    return sigmoid(x)/(1-sigmoid(x))
 
 
 examples = np.eye(8)
-x = np.vstack((np.ones(len(examples)), examples))
-y = np.arange(8)
+# examples = [[1, 0, 0, 0, 0, 0, 0, 0]]
+examplesBias = np.concatenate((np.ones(len(examples)).reshape(-1, 1), examples), axis=1)
+y = examples
 weightsIH = np.random.rand(9, 3)
 weightsHO = np.random.rand(4, 8)
 
@@ -17,11 +21,21 @@ GAMMA = 0.7
 # while not converged:
 # forward pass --> backprop
 # 8 nodes+bias X 3 nodes+bias X 8 nodes
-# print(np.sum(np.dot(x.T, weightsIH), axis=1))
-hiddenActivation = np.dot(x.T, weightsIH)
-print(hiddenActivation)
-hidden = sigmoid(hiddenActivation[0, 0])
+hiddenActivation = np.dot(examplesBias, weightsIH)
+outputHidden = sigmoid(hiddenActivation)
 
+# print(np.ones(len(examples)))
+outputHiddenBias = np.vstack([np.ones(len(examples)), outputHidden.T])
+yPred = np.dot(outputHiddenBias.T, weightsHO)
+yPred = sigmoid(yPred)
 
-print(hidden)
-# output = np.dot
+# print(yPred)
+# Backprop now
+deltaOutput = yPred - y
+# print(deltaOutput)
+# print(np.dot(outputHiddenBias, deltaOutput))
+deltaHidden = np.dot(np.dot(outputHiddenBias, deltaOutput), derivedSigmoid(yPred))
+
+print(deltaHidden)
+
+# TODO regularization
