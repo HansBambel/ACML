fec9f57673e0656c7f87d3f73663d04d4be96015
Kevin Trebing <Kevin.Trebing@gmx.net>
2018-11-01 17:42:36 +0100

    weights dont get updated correctly

--- sparse_autoencoder.py
+++ sparse_autoencoder.py
@@ -10,50 +10,66 @@ def derivedSigmoid(x):
 
 
 examples = np.eye(8)
-examples = [[1, 0, 0, 0, 0, 0, 0, 0]]
+# examples = [[1, 0, 0, 0, 0, 0, 0, 0]]
 y = examples
 weightsIH = np.random.rand(9, 3)
 weightsHO = np.random.rand(4, 8)
 # weightsIH = np.zeros((9, 3))
 # weightsHO = np.zeros((4, 8))
-
+# print(f'weightsIH {weightsIH}')
+batchSize= 4
+iterations = 0
 ALPHA = 0.01
 GAMMA = 0.7
-# while not converged:
-# forward pass --> backprop
-totalError = 0
-# 8 nodes+bias X 3 nodes+bias X 8 nodes
-inputBias = np.concatenate((np.ones(len(examples)).reshape(-1, 1), examples), axis=1)
-hiddenInput = np.dot(inputBias, weightsIH)
-print(f"hiddenInput.shape. Should be (numSamples, 3) {hiddenInput.shape}")
-activationHidden = sigmoid(hiddenInput)
-outputHiddenBias = np.concatenate((np.ones(len(examples)).reshape(-1, 1), activationHidden), axis=1)
-print(f"outputHiddenBias.shape. Should be (numSamples, 4) {outputHiddenBias.shape}")
+converged = False
+while not converged:
+    samples = examples[np.random.randint(len(examples), size=batchSize)]
+    # samples = np.random.permutation(examples)
+    # print(samples)
+    y = samples
+    iterations += 1
+    # forward pass --> backprop
+    totalError = 0
+    # 8 nodes+bias X 3 nodes+bias X 8 nodes
+    inputBias = np.concatenate((np.ones(len(samples)).reshape(-1, 1), samples), axis=1)
+    # hiddenInput = np.dot(inputBias, weightsIH)
+    # print(f"hiddenInput.shape. Should be (numSamples, 3) {hiddenInput.shape}")
+    activationHidden = sigmoid(np.dot(inputBias, weightsIH))
+    outputHiddenBias = np.concatenate((np.ones(len(samples)).reshape(-1, 1), activationHidden), axis=1)
+    # print(f"outputHiddenBias.shape. Should be (numSamples, 4) {outputHiddenBias.shape}")
 
-yPred = np.dot(outputHiddenBias, weightsHO)
-yPred = sigmoid(yPred)
-print("yPred")
-print(yPred)
+    yPred = sigmoid(np.dot(outputHiddenBias, weightsHO))
+    # print(f"yPred: {yPred}")
+    # print(f'y: {y}')
 
-# Backprop now
-outputError = yPred - y
-totalError += outputError
-print("outputError")
-print(outputError)
-print(outputError.shape)
-deltaOutput = outputError* derivedSigmoid(yPred)
-errorHidden = np.dot(deltaOutput, weightsHO.T)
-deltaHidden = errorHidden[:, 1:] * derivedSigmoid(activationHidden)
+    # Backprop now
+    outputError = yPred - y
+    # print("outputError")
+    # print(outputError)
+    # print(outputError.shape)
+    deltaOutput = outputError * derivedSigmoid(yPred)
+    # print(np.sum(outputError, axis=1))
+    totalError = np.mean(np.sum(outputError, axis=1))
 
-# delta2 = np.dot(outputError, weightsHO.T) * derivedSigmoid(outputHiddenBias)
-print(f"deltaOutput: {deltaOutput}")
-# delta1 = np.dot(delta2[:, 1:], weightsIH.T) * derivedSigmoid(inputBias)
-print(f"deltaHidden: {deltaHidden}")
-print(np.dot(yPred.T, deltaOutput).shape)
-weightsHO += ALPHA*np.dot(yPred.T, deltaOutput)
-weightsIH += ALPHA*np.dot(activationHidden.T, deltaHidden)
-# weightsIH += 1/len(examples)*(GAMMA*(weightsIH.T-ALPHA*(delta1.T)))
+    errorHidden = np.dot(deltaOutput, weightsHO.T)
+    deltaHidden = errorHidden[:, 1:] * derivedSigmoid(activationHidden)
 
+    # adapt the weights
+    weightsIH[1:, :] = 1/len(samples)*(ALPHA*np.dot(inputBias[:, 1:].T, deltaHidden) + GAMMA*weightsIH[1:, :])
+    weightsHO[1:, :] = 1/len(samples)*(ALPHA*np.dot(outputHiddenBias[:, 1:].T, deltaOutput) + GAMMA*weightsHO[1:, :])
+    # adapt the biases
+    weightsIH[0, :] = 1/len(samples)*(ALPHA*np.dot(inputBias[:, 0].T, deltaHidden))
+    weightsHO[0, :] = 1/len(samples)*(ALPHA*np.dot(outputHiddenBias[:, 0].T, deltaOutput))
+
+    # weightsIH += ALPHA*np.dot(inputBias.T, deltaHidden)
+    # weightsHO += ALPHA*np.dot(outputHiddenBias.T, deltaOutput)
+    # TODO regression
+    # weightsIH += 1/len(examples)*(GAMMA*(weightsIH.T-ALPHA*(delta1.T)))
+
+    print(totalError)
+    if iterations > 10:
+        break
+    if np.abs(totalError) <= 0.00001:
+        converged = True
 # print(delta1)
-
-# TODO regularization
+print(f'Converged after {iterations} iterations')
