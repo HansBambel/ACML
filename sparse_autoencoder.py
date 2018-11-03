7f7b4ab28a03fde1d8eeac70b20b17ca446340f7
Kevin Trebing <Kevin.Trebing@gmx.net>
2018-11-03 17:55:18 +0100

    regularization seems to work

--- sparse_autoencoder.py
+++ sparse_autoencoder.py
@@ -30,7 +30,7 @@ weightsHO = np.random.rand(4, 8)
 batchSize = 4
 iterations = 0
 ALPHA = 0.1
-LAMBDA = 0.3
+LAMBDA = 0.01
 converged = False
 while not converged:
 # while iterations < 100000:
@@ -57,31 +57,25 @@ while not converged:
         delta3 = yPred - sample
         # print(f'delta3 {delta3.shape}')
         # print(f'activationHidden {activationHidden.shape}')
-        # delta2 = np.dot(weightsHO, delta3.T) * derivedSigmoid(activationHidden)
         delta2 = np.multiply(np.dot(delta3, weightsHO.T), derivedSigmoid(activationHidden.T))
         # print(f'delta2 {delta2.shape}')
+        # print(LAMBDA*weightsHO[1:, :]**2)
         updateWeightsHO += np.dot(activationHidden, delta3)
         updateWeightsIH += np.dot(activationInput, delta2[:, 1:])
         totalError += np.sum(delta3)
+
     # print(f'updateWeightsIH: {updateWeightsIH}')
     # print(f'updateWeightsHO: {updateWeightsHO}')
-    # break
     m = len(samples)  # number of samples
     # update the bias weights
     weightsHO[0, :] -= ALPHA*updateWeightsHO[0, :]
     weightsIH[0, :] -= ALPHA*updateWeightsIH[0, :]
     # update the other weights
-    weightsHO[1:, :] -= ALPHA*updateWeightsHO[1:, :]
-    weightsIH[1:, :] -= ALPHA*updateWeightsIH[1:, :]
-    # weightsHO[1:, :] = (1 - ALPHA*LAMBDA/m)*weightsHO[1:, : ] - ALPHA*
-    # weightsIH[1:, :] = ALPHA/m * (updateWeightsIH[1:, :] + LAMBDA * weightsIH[1:, :])
-    # weightsHO[1:, :] = ALPHA/m * (updateWeightsHO[1:, :] + LAMBDA * weightsHO[1:, :])
-    # weightsIH[1:, :] = ALPHA/m * (updateWeightsIH[1:, :] + LAMBDA * weightsIH[1:, :])
+    weightsHO[1:, :] -= ALPHA*(updateWeightsHO[1:, :] + LAMBDA*weightsHO[1:, :])
+    weightsIH[1:, :] -= ALPHA*(updateWeightsIH[1:, :] + LAMBDA*weightsIH[1:, :])
 
     print(totalError)
-    # if iterations > 10:
-    #     break
-    if np.abs(totalError) <= 0.01:
+    if np.abs(totalError) <= 0.0001:
         converged = True
 # print(delta1)
 print(f'Converged after {iterations} iterations')
@@ -90,3 +84,6 @@ print('Testing:')
 for sample in examples:
     print(f'Input: {sample}')
     print(f'Output: {forwardPass(sample)}')
+
+print(f'weightsIH: \n {weightsIH}')
+print(f'weightsHO: \n {weightsHO}')
