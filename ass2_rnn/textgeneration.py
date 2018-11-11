5b7ed849cc5677c7047bd2b085c2d7e282115463
Kevin Trebing <Kevin.Trebing@gmx.net>
2018-11-11 18:43:15 +0100

    added training script

--- ass2_rnn/textgeneration.py
+++ ass2_rnn/textgeneration.py
@@ -4,6 +4,6 @@ textgen = textgenrnn(weights_path='weights/nips-abstracts_weights.hdf5',
                      vocab_path='weights/nips-abstracts_vocab.json',
                      config_path='weights/nips-abstracts_config.json')
 
-textgen.generate(7, temperature=0.6)
+textgen.generate(7, temperature=0.4)
 
 # We consider the problem of user prediction has been proposed to achieve substantial descriptions in the weighted probabilistic model of the maximum likelihood estimator.
